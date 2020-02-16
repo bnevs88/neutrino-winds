8354f072cd666ab5ba14a0b4b0b515294d00a8f0
bnevs88 <bnevins108@gmail.com>
2020-02-16 15:13:07 -0500

    Made RK step adaptive and tested

--- C++/NDWSolver/NDWSolver/Integrator.h
+++ C++/NDWSolver/NDWSolver/Integrator.h
@@ -15,7 +15,7 @@ private:
 	int size;
 	
 public:
-	double* coupledRKStep(double t, double dt, double state[]); //the output will be a pointer to an array
+	double* coupledRKStep(double t, double dt0, double state[]); //the output will be a pointer to an array
 	Integrator(double initialState[], Equations equations);
 	vector<double> generateFunction(double initialState[]); 
 };
@@ -29,30 +29,48 @@ Integrator::Integrator(double initialState[], Equations equations)
 	eqs = equations;
 }
 
-double* Integrator::coupledRKStep(double t, double dt, double state[])
+double* Integrator::coupledRKStep(double t, double dt0, double state[])//adaptive RK4 step
 {
 	double karr[3][4] = { { 0,0,0,0 },{ 0,0,0,0 },{ 0,0,0,0 } };
 	double* step=new double[3];
-	for (int j = 0; j < 4; j++) { //j iterates through k1,k2,k3,k4 for the RK4 step
-		if (j == 0) {
-			karr[0][j] = dt * eqs.dx(t, state);
-			karr[1][j] = dt * eqs.du(t, state);
-			karr[2][j] = dt * eqs.dw(t, state);
-		}
-		else {
-			//generate new state array to pass to functions
-			double newstate[3] = { 0,0,0 };
-			for (int k = 0; k < 3; k++) {
-				newstate[k] = state[k] + karr[k][j - 1] / 2;
+	double dt = dt0;
+	double pc = 100;
+	double statenorm = 0.;
+	for (int i = 0; i < 3; i++) {
+		statenorm += pow(state[i],2);
+	}
+	statenorm = sqrt(statenorm);
+	while (pc > 1 or pc < .1) {
+		for (int j = 0; j < 4; j++) { //j iterates through k1,k2,k3,k4 for the RK4 step
+			if (j == 0) {
+				karr[0][j] = dt * eqs.dx(t, state);
+				karr[1][j] = dt * eqs.du(t, state);
+				karr[2][j] = dt * eqs.dw(t, state);
 			}
+			else {
+				//generate new state array to pass to functions
+				double newstate[3] = { 0,0,0 };
+				for (int k = 0; k < 3; k++) {
+					newstate[k] = state[k] + karr[k][j - 1] / 2;
+				}
 
-			karr[0][j] = dt * eqs.dx(t + dt / 2, newstate);
-			karr[1][j] = dt * eqs.du(t + dt / 2, newstate);
-			karr[2][j] = dt * eqs.dw(t + dt / 2, newstate);
-			for (int i = 0; i < 3; i++) {
-				step[i] = state[i] + (karr[i][0] + 2 * karr[i][1] + 2 * karr[i][2] + karr[i][3]) / 6;
+				karr[0][j] = dt * eqs.dx(t + dt / 2, newstate);
+				karr[1][j] = dt * eqs.du(t + dt / 2, newstate);
+				karr[2][j] = dt * eqs.dw(t + dt / 2, newstate);
+				for (int i = 0; i < 3; i++) {
+					step[i] = state[i] + (karr[i][0] + 2 * karr[i][1] + 2 * karr[i][2] + karr[i][3]) / 6;
+				}
 			}
 		}
+		
+		double stepnorm = 0.;
+		for (int i = 0; i < 3; i++) {
+			stepnorm += pow(step[i] - state[i], 2);
+		}
+		stepnorm = sqrt(stepnorm);
+		pc = 100 * stepnorm / statenorm;
+		if (pc > 1) { dt = dt / 2; }
+		else if (pc < .1) { dt = 2 * dt; };
 	}
 	cout << "\nStep:\n";
 	for (int i = 0; i < 3; i++) {
--- C++/NDWSolver/NDWSolver/tester.cpp
+++ C++/NDWSolver/NDWSolver/tester.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main() {
 	cout << "\nHello world\n";
 	Equations eqs;
-	double state[3] = { 1,-2,1 };
+	double state[3] = { 1,1,1 };
 	Integrator In(state, eqs);
 	cout << eqs.dx(0, state) <<", "<< eqs.du(0, state)<<", "<< eqs.dw(0, state);
 	double *step = In.coupledRKStep(0, .1, state);
@@ -16,5 +16,26 @@ int main() {
 	for (int i = 0; i < 3; i++) {
 		cout << step[i] << "\n";
 	}
+
+	double step2[3] = { 1.2,1.2,1.2 };
+	double pc = 100;
+	double statenorm = 0.;
+	for (int i = 0; i < 3; i++) {
+		statenorm += pow(state[i], 2);
+	}
+	statenorm = sqrt(statenorm);
+	double stepnorm = 0.;
+	
+	for (int i = 0; i < 3; i++) {
+		cout << "State: " << state[i] << endl;
+		stepnorm += pow(step[i] - state[i], 2);
+		cout << "added " << step[i] - state[i] << endl;
+	}
+	stepnorm = sqrt(stepnorm);
+	pc = 100 * stepnorm / statenorm;
+	cout << "statenorm: " << statenorm << endl;
+	cout << "stepnorm: " << stepnorm << endl;
+	cout << "Percent change: " << pc << endl;
+
 	return 0;
 }
