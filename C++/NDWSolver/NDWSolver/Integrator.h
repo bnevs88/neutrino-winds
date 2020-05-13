ea545a3f8567de5d80a1a4757ae839e81e99a44c
bnevs88 <bnevins108@gmail.com>
2020-05-13 17:25:46 -0400

    made resolution adaptive to increase accuracy of v0

--- C++/NDWSolver/NDWSolver/Integrator.h
+++ C++/NDWSolver/NDWSolver/Integrator.h
@@ -11,22 +11,27 @@ private:
 	vector<double*> data;
 	Equations eqs;
 	int statelen;
+	double res;
+	double vcrit;
 	
 public:
 	double* coupledRKStep(double dt0, double state[], int itermax); //the output will be a pointer to an array
-	Integrator(Equations equations);
+	Integrator(Equations equations, double resolution);
 	vector<double*> generateFunction(double initialState[], double xrange, double urange, int itermax); 
 	void writeToFile(string fileName);
 	double findZeros(double initialState[], int itermax);
-	double findV0(double maxprecision, int itermax);
-	void scan(string fileName, double lower, double upper, int N);
+	double checkZeros(double initialState[], int itermax);
+	double findV0(double tol, double maxprecision, int itermax);
+	void scan(string fileName, double lower, double upper, int N, double tol);
 	void printState(double state[]);
 };
 
-Integrator::Integrator(Equations equations)
+Integrator::Integrator(Equations equations, double resolution = 1.)
 {
 	eqs = equations;
 	statelen = 4;
+	res = resolution;
+	vcrit = 0;
 }
 
 void Integrator::printState(double state[])
@@ -43,7 +48,6 @@ double* Integrator::coupledRKStep(double dt0, double state[], int itermax=100000
 	double* step=new double[4];
 	double dt = dt0;
 	double pc = 100;
-	double range = .01;
 	double statenorm = 0.;
 	for (int i = 1; i < 4; i++) {
 		statenorm += pow(state[i],2);
@@ -51,7 +55,7 @@ double* Integrator::coupledRKStep(double dt0, double state[], int itermax=100000
 	statenorm = sqrt(statenorm);
 	//cout << "Statenorm: " << statenorm << endl;
 	int it = 0;
-	while ((pc > range or pc < range/10.) and it<itermax) {
+	while ((pc > res or pc < res/10.) and it<itermax) {
 		for (int j = 0; j < 4; j++) { //j iterates through k1,k2,k3,k4 for the RK4 step
 			if (j == 0) {
 				karr[0][j] = dt * eqs.dx(state);
@@ -95,8 +99,8 @@ double* Integrator::coupledRKStep(double dt0, double state[], int itermax=100000
 		}
 		stepnorm = sqrt(stepnorm);
 		pc = 100 * stepnorm / statenorm;
-		if (pc > range) { dt = dt / 2.; }
-		else if (pc < range/10.) { dt = 1.9 * dt; };
+		if (pc > res) { dt = dt / 2.; }
+		else if (pc < res/10.) { dt = 1.9 * dt; };
 		it++;
 	}
 	if (it >= itermax) { cout << "Max iteration count exceeded in coupledRKStep, pc=" << pc << endl; printState(step); };
@@ -180,30 +184,91 @@ double Integrator::findZeros(double initialState[], int itermax=100000)
 	return tu - tx;
 }
 
-double Integrator::findV0(double maxprecision = 1E-8, int itermax = 10000) 
+double Integrator::checkZeros(double initialState[], int itermax = 100000) 
 {
-	double v0 = .9;
-	double dv = -.4;
-	double testState[4] = { 0,0,0,0 };
+	data.clear();
+	data.push_back(initialState);
 	int i = 0;
-	//cout << "Finding v0" << endl;
-	while (abs(dv) > maxprecision&& i < itermax) {
-		
-		while ((copysign(1, findZeros(new double[4] { 0,0,log(v0 + dv),0 }))!= copysign(1, findZeros(new double[4]{ 0,0,log(v0),0 }))) or (v0+dv>1) or (v0+dv<0)) {
-			//cout << "v0: " << v0 << ", dv: " << dv << endl;
-			dv = dv / 2;
+	double tu = 0;
+	double tx = 0;
+	//cout << "finding zeros" << endl;
+	while ((tx == 0 || tu == 0) && exp(data.back()[1]) < 500) {
+		//cout << "r=" << exp(data.back()[1]) << endl;
+		double* step = coupledRKStep(.01, data.back());
+		/*cout << "state for f1: ";
+		printState(data.back());
+		cout << "step for f1: ";
+		printState(step);
+		cout << "current f1: " << eqs.ndf1(data.back()) << ", next f1: " << eqs.ndf1(step) << endl;*/
+		if (copysign(1., eqs.ndf1(step)) != copysign(1., eqs.ndf1(data.back()))) {
+			tu = step[0];
+			//cout << "found tu=" << tu << endl;
 		};
-		v0 = v0 + dv;
-		//cout << "v0: " << v0 << ", dv: " << dv << endl;
-		//cout << "tu-tx = " << findZeros(new double[4]{ 0, 0, log(v0), 0 }) << endl;
+		if (copysign(1., eqs.ndf2(step)) != copysign(1., eqs.ndf2(data.back()))) {
+			tx = step[0];
+			//cout << "found tx=" << tx << endl;
+		};
+		data.push_back(step);
 		i++;
-		
 	};
-	if (i >= itermax) { cout << "Max iteration count exceeded in findV0" << endl; };
-	return v0 + dv;
+	if (tx == 0 || tu == 0) {
+		cout << "Exceeded max integration length in findZeros" << endl;
+		/*for (int j = 0; j < 4; j++) {
+			cout << initialState[j] << endl;
+		}*/
+	}
+	//cout << "tx: " << tx << ", tu: " << tu << ", tu-tx=" << tu - tx << endl;
+	return abs(tu - tx);
 }
 
-void Integrator::scan(string fileName, double lower = .001, double upper = .01, int N = 10)
+double Integrator::findV0(double tol = .9, double maxprecision = 1E-8, int itermax = 10000)
+{
+	if (vcrit == 0) {
+		double v0 = .9;
+		double dv = -.4;
+		double testState[4] = { 0,0,0,0 };
+		int i = 0;
+		double err = 0;
+		double prev = 0;
+		bool exit = false;
+		//cout << "Finding v0" << endl;
+		do {
+			while (abs(dv) > maxprecision&& i < itermax) {
+
+				while ((copysign(1, findZeros(new double[4]{ 0,0,log(v0 + dv),0 })) != copysign(1, findZeros(new double[4]{ 0,0,log(v0),0 }))) or (v0 + dv > 1) or (v0 + dv < 0)) {
+					//cout << "v0: " << v0 << ", dv: " << dv << endl;
+					dv = dv / 2;
+				};
+				v0 = v0 + dv;
+				//cout << "v0: " << v0 << ", dv: " << dv << endl;
+				//cout << "tu-tx = " << findZeros(new double[4]{ 0, 0, log(v0), 0 }) << endl;
+				i++;
+
+			};
+			err = checkZeros(new double[4]{ 0,0,log(v0 + dv),0 });
+			if (err > tol) {
+				if (100 * abs(prev - v0) / v0 > .5) {
+					res = res / 2.;
+					cout << "v0 is " << v0 + dv << ", error is " << err << ", increasing resolution to " << res << endl;
+					prev = v0 + dv;
+					v0 = .9;
+					dv = -.4;
+				}
+				else {
+					exit = true;
+				};
+			}
+			else {
+				exit = true;
+			};
+		} while (!exit);
+		if (i >= itermax) { cout << "Max iteration count exceeded in findV0" << endl; };
+		vcrit = v0 + dv;
+	}
+	return vcrit;
+}
+
+void Integrator::scan(string fileName, double lower = .001, double upper = .01, int N = 10, double tol = .9)
 {
 	ofstream output(fileName);
 	for (double v = lower; v < upper; v = v + (upper-lower)/N) {
@@ -217,7 +282,7 @@ void Integrator::scan(string fileName, double lower = .001, double upper = .01, 
 		}
 		d.clear();
 	}
-	double v0 = findV0();
+	double v0 = findV0(tol);
 	cout << "Making data for v=" << v0 << endl;
 	vector<double*> d = generateFunction(new double[4]{ 0,0,log(v0),0 }, 50.);
 	for (int i = 0; i < d.size(); i++) {
--- C++/NDWSolver/NDWSolver/tester.cpp
+++ C++/NDWSolver/NDWSolver/tester.cpp
@@ -8,15 +8,15 @@
 using namespace std;
 
 int main() {
-	Equations eqs(1.01,10,200,1);
+	Equations eqs(1.01,10,200,2);
 	//cout << "Compiled equations" << endl;
 	//Equations eqs;
 	Integrator In(eqs);
 
-	//In.generateFunction(new double[4]{ 0,0,log(1E-8),0 }, 50);
+	//In.generateFunction(new double[4]{ 0,0,log(0.000379538),0 }, 50);
 	//In.writeToFile("./output.txt");
 	//cout << In.findZeros(new double[4]{ 0,0,log(.000353531 - 2.98023E-8),0 }) << endl;
-	//cout << In.findZeros(new double[4]{ 0,0,log(.000353531),0 }) << endl;
+	//cout << In.checkZeros(new double[4]{ 0,0,log(0.000379538),0 }) << endl;
 	
 	//cout << "Compiled Integrator" << endl;
 	//double* teststep = In.coupledRKStep(.1, new double[4]{ 1,1,1,1 });
@@ -29,7 +29,7 @@ int main() {
 	//cout << "found v0" << endl;
 	//cout << "finding v0" << endl;
 	clock_t timereq = clock();
-	double v0 = In.findV0();
+	double v0 = In.findV0(.894);
 	timereq = clock() - timereq;
 	cout << "v0 = " << v0 << ", time elapsed = " << (float)timereq/CLOCKS_PER_SEC << endl;
 	//cout << In.findZeros(new double[4]{ 0,0,log(v0),0 }) << endl;
@@ -39,11 +39,11 @@ int main() {
 	//	cout << data.back()[i] << endl;
 	//}
 	//In.generateFunction(new double[4]{ 0,0,log(.0003755),0 },50);
-	//In.generateFunction(new double[4]{ 0,0,log(v0),0 }, 50);
-	//In.writeToFile("./output.txt");
+	In.generateFunction(new double[4]{ 0,0,log(v0),0 }, 50);
+	In.writeToFile("./output.txt");
 	//cout << In.findZeros(new double[4]{ 0,0,log(.0003755),0 }) << endl;
 	//cout << "scanning" << endl;
-	In.scan("./gammahc.txt",.0003,.0004,10);
+	In.scan("./gammahc.txt",v0/2.,2*v0,10,.89);
 	return 0;
 }
 
